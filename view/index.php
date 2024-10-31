cadd04bec3731372a5dc5430b2ba45dccabda1ac
Vitor-Tx <vitor.teixeira267@gmail.com>
2024-10-31 20:49:20 -0300

    fix carousel

--- view/index.php
+++ view/index.php
@@ -33,7 +33,7 @@
     <div id="courseCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="./assets/images/banner.jpeg" class="d-block w-100" alt="Carousel Image 1" style="object-fit: cover; object-position: center;">
+                <img src="./assets/images/banner.jpeg" class="d-block w-100" alt="Carousel Image 1">
                 <div class="carousel-caption d-none d-md-block text-start">
                     <h5 class="text-uppercase">Lorem Ipsum</h5>
                     <p>Aenean lacinia bibendum nulla sed consectetur...</p>
